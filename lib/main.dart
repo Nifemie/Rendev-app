05d8af9223410555a3ecdf936411a7fd44eb09d8
Nifemie <nifemiolowu2@gmail.com>
2025-04-18 18:10:31 +0100

    Added modern login UI with styling and validation

--- lib/main.dart
+++ lib/main.dart
@@ -10,54 +10,132 @@ class MyApp extends StatelessWidget {
 }
 
 class LoginPage extends StatelessWidget {
-  final _formkey = GlobalKey<FormState>();
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController firstnameController = TextEditingController();
+  final TextEditingController lastnameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Form(
-          key: _formkey,
-          child: Column(
-            children: [
-              Text(
-                "LOGIN",
-                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFF6A11CB), Color(0xFF2575FC)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(24.0),
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(64),
               ),
-              SizedBox(height: 32),
-              TextFormField(
-                controller: _emailController,
-                decoration: InputDecoration(labelText: 'Email'),
-                keyboardType: TextInputType.emailAddress,
-                validator:
-                    (Value) =>
-                        Value!.isEmpty ? 'Please enter your email' : null,
+              elevation: 10,
+              child: Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        'WELCOME BACK',
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue[800],
+                        ),
+                      ),
+                      const SizedBox(height: 24),
+                      TextFormField(
+                        controller: emailController,
+                        decoration: const InputDecoration(
+                          labelText: 'Email',
+                          prefixIcon: Icon(Icons.email),
+                          border: OutlineInputBorder(),
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                        validator:
+                            (value) =>
+                                value!.isEmpty ? 'Enter your email' : null,
+                      ),
+                      const SizedBox(height: 16),
+                      TextFormField(
+                        controller: passwordController,
+                        decoration: const InputDecoration(
+                          labelText: 'Password',
+                          prefixIcon: Icon(Icons.lock),
+                          border: OutlineInputBorder(),
+                        ),
+                        obscureText: true,
+                        validator:
+                            (value) =>
+                                value!.isEmpty ? 'Enter your password' : null,
+                      ),
+                      const SizedBox(height: 24),
+                      TextFormField(
+                        controller: firstnameController,
+                        decoration: const InputDecoration(
+                          labelText: 'FirstName',
+                          prefixIcon: Icon(Icons.person),
+                          border: OutlineInputBorder(),
+                        ),
+                        keyboardType: TextInputType.name,
+                        validator:
+                            (value) =>
+                                value!.isEmpty ? 'Enter your firstname' : null,
+                      ),
+                      const SizedBox(height: 16),
+                      TextFormField(
+                        controller: lastnameController,
+                        decoration: const InputDecoration(
+                          labelText: 'LastName',
+                          prefixIcon: Icon(Icons.person),
+                          border: OutlineInputBorder(),
+                        ),
+                        keyboardType: TextInputType.name,
+                        validator:
+                            (value) =>
+                                value!.isEmpty ? 'Enter your lastname' : null,
+                      ),
+                      const SizedBox(height: 16),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              print(
+                                'Email: ${emailController.text}, Password: ${passwordController.text},name: ${firstnameController.text},name: ${lastnameController.text},',
+                              );
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(vertical: 14),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                          child: const Text(
+                            'Login',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 12),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text("Don't have an account? Sign up"),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              SizedBox(height: 16),
-              TextFormField(
-                controller: _passwordController,
-                decoration: InputDecoration(labelText: 'Password'),
-                obscureText: true,
-                validator:
-                    (Value) =>
-                        Value!.isEmpty ? 'Please enter your password' : null,
-              ),
-              SizedBox(height: 24),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formkey.currentState!.validate()) {
-                    // Perform login action
-                    print('Email: ${_emailController.text}');
-                    print('Password: ${_passwordController.text}');
-                  }
-                },
-                child: Text('Login'),
-              ),
-            ],
+            ),
           ),
         ),
       ),
